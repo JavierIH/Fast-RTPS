d4f7f78cc509106379ad429b8737c6bf1c89139d
JavierIH <javierisabel@eprosima.com>
2017-02-20 09:42:21 +0100

    Refs #1945. Reviewed StringMatching and added unit tests.
    Reviewed code related with winrt

--- src/cpp/utils/StringMatching.cpp
+++ src/cpp/utils/StringMatching.cpp
@@ -95,9 +95,9 @@ bool StringMatching::matchString(const char* str1, const char* str2)
 #else
 bool StringMatching::matchString(const char* str1, const char* str2)
 {
-	if(fnmatch(str1,str2,FNM_PATHNAME)==0)
+	if(fnmatch(str1,str2,FNM_NOESCAPE)==0)
 		return true;
-	if(fnmatch(str2,str1,FNM_PATHNAME)==0)
+	if(fnmatch(str2,str1,FNM_NOESCAPE)==0)
 		return true;
 	return false;
 }
